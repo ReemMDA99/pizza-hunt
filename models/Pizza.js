c99b36478bbb3e1308cc5151df8c15caae248db1
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-11 18:39:36 -0600

    add routes and test

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,8 @@
 const { Schema, model } = require('mongoose');
+// create the Pizza model using the PizzaSchema
+const Pizza = require('./Pizza');
+const Comment = require('./Comment');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,17 +13,33 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
-    
-});
-// create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
-// export the Pizza model
-module.exports = Pizza;
+
+module.exports = { Pizza, Comment };
